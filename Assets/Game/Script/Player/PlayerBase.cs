2eaa8321c3853d3010628b07bd1f8989f87ea06b
sakamotosyo <syo10150000@gmail.com>
2022-06-23 23:29:07 +0900

    PlayerBaseの追加

---
+++ Assets/Game/Script/Player/PlayerBase.cs
@@ -0,0 +1,126 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBase : MonoBehaviour, IDamageble
+{
+    public enum MoveType
+    {
+        WaitKey,
+        Move,
+    }
+
+    [SerializeField, Header("最大HP")] private int _maxHp;
+    [SerializeField, Header("HP")] private float _playerHp;
+    [SerializeField, Header("攻撃力")] private float _power;
+    [SerializeField, Header("行動の回数")] private int _actionNum;
+
+    [Tooltip("ゲームマネージャー")]
+    private GameManager _gameManagerIns;
+
+    [Tooltip("プレイヤーが次に移動する場所")]
+    private Vector3 _nextPosition;
+
+    [Tooltip("動作中かどうか")]
+    private bool _isMoving;
+    //プレイヤーのムーブタイプ
+    private MoveType _moveType = MoveType.WaitKey;
+
+    //Test用
+    private float _waitTime = 0.1f;
+    private float _countTime = 0;
+
+    private void Start()
+    {
+        _gameManagerIns = GameManager.Instance;
+        _gameManagerIns.PlayerX = (int)transform.position.x;
+        _gameManagerIns.PlayerY = -1 * (int)transform.position.y;
+
+    }
+    private void Update()
+    {
+        if (_gameManagerIns.TurnType == GameManager.TurnManager.Player && _waitTime < _countTime)
+        {
+            InputKey();
+        }
+        _countTime += Time.deltaTime;
+    }
+
+    /// <summary>
+    /// 移動の入力処理
+    /// </summary>
+    private void InputKey()
+    {
+        _countTime = 0;
+        var x = Input.GetAxisRaw("Horizontal");
+        var y = Input.GetAxisRaw("Vertical");
+        //シフトを押しているときは移動できなくする
+        if (!Input.GetKey(KeyCode.LeftShift))
+        {
+            _isMoving = judgeMove((int)x, (int)y);
+            //移動先に障害物がないかどうか
+            if (_isMoving && (x != 0 || y != 0))
+            {
+                //ここに岩や敵があった時移動できないという処理を追加する
+                _nextPosition = transform.position + new Vector3(x, y, 0);
+
+                //ゲームマネージャーにプレイヤーの場所を渡す
+                GameManager.Instance.PlayerX += (int)x;
+                GameManager.Instance.PlayerY += (int)y * -1;
+
+                transform.position = Vector3.Lerp(transform.position, _nextPosition, 1);
+
+                GameManager.Instance.TurnType = GameManager.TurnManager.Enemy;
+            }
+            else
+            {
+
+            }
+        }
+        else
+        {
+            Debug.Log("Shift");
+            ////ここに岩や敵があった時移動できないという処理を追加する
+            //_nextPosition = transform.position + new Vector3(x, y, 0);
+            //_moveType = MoveType.Move;
+        }
+
+        _isMoving = false;
+    }
+
+    /// <summary>
+    /// ダンジョンの区画にアクセスして次の場所が移動可能か調べる
+    /// </summary>
+    /// <param name="x"></param>
+    /// <param name="y"></param>
+    /// <returns></returns>
+    private bool judgeMove(int x, int y)
+    {
+        //マップデータにアクセス
+        int value = DgGenerator.Instance.Layer.GetMapData(_gameManagerIns.PlayerX + x, _gameManagerIns.PlayerY + y * -1);
+
+        // Debug.Log(value);
+        //0は壁、1は道
+        if (value == 0)
+        {
+            Debug.Log("かべ");
+            return false;
+
+        }
+        else if (value == 1)
+        {
+            Debug.Log("呼ばれた");
+            return true;
+
+        }
+
+        return false;
+
+    }
+
+    public void AddDamage(float damage) 
+    {
+        Debug.Log($"プレイヤーは{damage}のダメージを受けた");
+        _playerHp -= damage;
+    }
+}
