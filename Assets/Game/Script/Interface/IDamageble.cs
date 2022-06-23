a56e634a0b0e3c938985d20a7242a9174c817e55
sakamotosyo <syo10150000@gmail.com>
2022-06-23 23:26:46 +0900

    ダメージを与える処理をするインターフェース

---
+++ Assets/Game/Script/Interface/IDamageble.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDamageble
+{
+    public void AddDamage(float damage);
+    //‚±‚±‚É‰½‘®«‚©“ü‚ê‚Ä‚à‚¢‚¢‚©‚à
+}
