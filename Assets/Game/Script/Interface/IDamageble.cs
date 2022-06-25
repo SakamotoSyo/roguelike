43f461bc0a316062293a6fbea0ecca269c4136c0
sakamotosyo <syo10150000@gmail.com>
2022-06-25 23:04:37 +0900

    テストenumの追加

--- Assets/Game/Script/Interface/IDamageble.cs
+++ Assets/Game/Script/Interface/IDamageble.cs
@@ -2,8 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum ChType
+{
+    Player,
+    Enemy
+}
+
 public interface IDamageble
 {
+    ChType GetType { get; }
+
     public void AddDamage(float damage);
     //‚±‚±‚É‰½‘®«‚©“ü‚ê‚Ä‚à‚¢‚¢‚©‚à
 }
