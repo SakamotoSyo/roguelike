b80f04381566f43788bf1a4d22ce108fc8f6e3b5
sakamotosyo <syo10150000@gmail.com>
2022-06-29 23:31:32 +0900

    時間余ったら使いたい……

---
+++ Assets/Game/Script/Test/ObjectPoolTest.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "ObjectPoolData")]
+public class ObjectPoolTest : ScriptableObject
+{
+   
+   
+}
