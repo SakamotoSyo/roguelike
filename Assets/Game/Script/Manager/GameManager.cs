0e224484fb7c1969845cc277303748414c201b1d
sakamotosyo <syo10150000@gmail.com>
2022-06-29 23:23:52 +0900

    メニューをオープンしたときのタイプの追加と変数をカプセル化

--- Assets/Game/Script/Manager/GameManager.cs
+++ Assets/Game/Script/Manager/GameManager.cs
@@ -7,15 +7,43 @@ public class GameManager : SingletonBehaviour<GameManager>
     public enum TurnManager
     {
         Player,
+        MenuOpen,
         Enemy,
     }
 
     public TurnManager TurnType;
 
-    public GameObject PlayerBase;
+    private GameObject _playerObj;
+    public GameObject PlayerObj => _playerObj;
 
-    //プレイヤーの場所
-    public int PlayerX;
-    public int PlayerY;
- 
+
+    //プレイヤーの座標
+    private int _playerX;
+    private int _playerY;
+    //カプセル化
+    public int PlayerX => _playerX;
+    public int PlayerY => _playerY;
+    
+
+
+    /// <summary>
+    /// プレイヤーのポジションをセットする
+    /// </summary>
+    /// <param name="x">x座標</param>
+    /// <param name="y">y座標</param>
+    public void SetPlayerPosition(int x ,int y)
+    {
+        _playerX += x;
+        _playerY += y;
+    }
+
+    /// <summary>
+    /// プレイヤーの座標をセットする
+    /// </summary>
+    /// <param name="player">playerのオブジェクト</param>
+    public void SetPlayerObj(GameObject player) 
+    {
+        _playerObj = player;
+    }
+
 }
