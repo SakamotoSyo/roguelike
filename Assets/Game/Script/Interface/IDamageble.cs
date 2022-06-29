00feeb61d1e1d26a84b337f330f169974a9ed0c9
sakamotosyo <syo10150000@gmail.com>
2022-06-29 23:20:09 +0900

    インターフェースのenumを使わないのでコメント化

--- Assets/Game/Script/Interface/IDamageble.cs
+++ Assets/Game/Script/Interface/IDamageble.cs
@@ -2,15 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum ChType
-{
-    Player,
-    Enemy
-}
+//public enum ChType
+//{
+//    Player,
+//    Enemy
+//}
 
 public interface IDamageble
 {
-    ChType GetType { get; }
+    //ChType GetType { get; }
 
     public void AddDamage(float damage);
     //‚±‚±‚É‰½‘®«‚©“ü‚ê‚Ä‚à‚¢‚¢‚©‚à
