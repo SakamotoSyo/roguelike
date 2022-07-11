51b40cf4f8a2b1ca92f6b723fa6f16562ed0327d
sakamotosyo <syo10150000@gmail.com>
2022-07-11 17:22:45 +0900

    時間が余ったらやるやる

---
+++ Assets/Game/Script/Test/Daiksutora.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//public class Daiksutora : MonoBehaviour
+//{
+//    public int _node = 1;
+
+//    //public List
+
+//    //移動先の頂点とコストを格納する辺のデータ
+//    public struct Edge
+//    {
+//        public int To;
+//        public long Cost;
+
+//        public Edge(int to, long cost)
+//        {
+//            this.To = to;
+//            this.Cost = cost;
+//        }
+//    }
+
+//    public struct P : lComparable<P> 
+//    {
+
+//    } 
+//}
+
+//class PriorityQueue<T> where T : lComparable<T>
+//{
+
+//}
+
+
