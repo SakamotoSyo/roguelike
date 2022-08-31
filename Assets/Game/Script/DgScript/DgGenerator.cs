463ffc6ad4cf2c67e0fb7fd4cade8baecc1a3b73
sakamotosyou <syo10150000@gmail.com>
2022-09-01 01:00:32 +0900

    マップの再生成が可能なようにActionを追加

--- Assets/Game/Script/DgScript/DgGenerator.cs
+++ Assets/Game/Script/DgScript/DgGenerator.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Tilemaps;
 
 //メモ=>
 //1. 初期化 (2次元配列作成・区画リスト作成)
@@ -16,7 +15,6 @@ using UnityEngine.Tilemaps;
 /// </summary>
 public class DgGenerator : SingletonBehaviour<DgGenerator>
 {
-
     //2次元配列情報
     public Layer2D Layer = null;
 
@@ -24,6 +22,9 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
     private List<DgDivision> _divList = null;
 
     public List<DgDivision> DivList => _divList;
+
+    /// <summary>マップの再生成を通知する</summary>
+    public event System.Action MapNotice;
 
     [Header("高さを設定する")]
     [SerializeField] private int _height;
@@ -57,13 +58,16 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
 
     //[Header("壁のタイルチップ")]
     //[SerializeField] private Tile _wallTileChip;
-    
+
     //[Header("階段のタイルチップ")]
     //[SerializeField]private Tile _stairTileChip;
     #endregion
 
+    [Header("Objectをまとめる親のPrefab")]
+    [SerializeField] GameObject _mapParentPrefab;
+
     [Header("グラウンドのオブジェクト")]
-    [SerializeField]private GameObject _kusanyaObject;
+    [SerializeField] private GameObject _kusanyaObject;
 
     [Header("岩のオブジェクト")]
     [SerializeField] private GameObject _iwaObject;
@@ -77,7 +81,7 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
     [Header("敵のプレハブ")]
     [SerializeField] private GameObject _enemyPrefab;
 
-    [Header("トラップのプレハブ")] 
+    [Header("トラップのプレハブ")]
     [SerializeField] private GameObject _trapPrefab;
 
     [Header("ItemDataBase")]
@@ -86,15 +90,32 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
     [Header("Itemのプレハブ")]
     [SerializeField] private GameObject _itemPrefab;
 
+    [Tooltip("mapParentを生成したものを保存する")]
+    GameObject _mapParent;
+
     [Tooltip("マップを生成し終わったかどうか")]
     private bool _mapGenerateEnd;
     public bool MapGenerateEnd => _mapGenerateEnd;
 
     private bool isVertical = false;
+
+    public const int StairSet = 3;
     //縦で分割するかどうか
     // Start is called before the first frame update
     protected override void OnAwake()
     {
+        MapNotice += MapInit;
+        MapGeneration();
+    }
+
+    /// <summary>
+    /// マップを生成する処理
+    /// </summary>
+    public void MapGeneration() 
+    {
+        //マップの再生成による設定の初期化
+        MapNotice();
+
         _mapGenerateEnd = false;
 
         //二次元配列に値を入れる
@@ -104,7 +125,7 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
         _divList = new List<DgDivision>();
 
         //すべてを壁にする
-        Layer.Fill(_chipWall);
+        Layer.Fill(MapNum.WallNum);
 
         //最初の区画を作る
         CreateDivision(0, 0, _width - 1, _height - 1);
@@ -122,6 +143,7 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
         SetTile();
     }
 
+
     /// <summary>
     /// 区画情報をリストに保存する
     /// </summary>
@@ -129,7 +151,7 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
     /// <param name="top"></param>
     /// <param name="right"></param>
     /// <param name="bottom"></param>
-    void CreateDivision(int left, int top, int right, int bottom) 
+    void CreateDivision(int left, int top, int right, int bottom)
     {
         //矩形の情報をセットする
         DgDivision div = new DgDivision();
@@ -140,7 +162,7 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
     /// <summary>
     /// 区画を分割する
     /// </summary>
-    private void SplitDivison(bool isVertical)  
+    private void SplitDivison(bool isVertical)
     {
         //末尾要素の取り出し
         DgDivision parent = _divList[_divList.Count - 1];
@@ -148,7 +170,7 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
         //子区画の要素を作る
         DgDivision child = new DgDivision();
 
-        if (parent.Outer.Left + _minRoom + _outerMergin >= parent.Outer.Right - _minRoom - _outerMergin || parent.Outer.Top + _minRoom + _outerMergin >= parent.Outer.Bottom - _minRoom -_outerMergin) 
+        if (parent.Outer.Left + _minRoom + _outerMergin >= parent.Outer.Right - _minRoom - _outerMergin || parent.Outer.Top + _minRoom + _outerMergin >= parent.Outer.Bottom - _minRoom - _outerMergin)
         {
             //どちらかの線の長さが足りない場合親の区画を元に戻しておしまい
             //どちらも最大数まで分けると形が一定になるのでランダム要素をつけるためにどちらかの線の長さが足りない場合にする
@@ -177,7 +199,7 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
             //親のLeftを子区画の右端までに移動させる
             parent.Outer.Right = child.Outer.Left;
         }
-        else 
+        else
         {
             //分割ポイントを求める*Topの初期値は０
             int a = parent.Outer.Top + (_minRoom + _outerMergin);
@@ -197,18 +219,18 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
             //親のTopを子区画の右端までに移動させる
             parent.Outer.Bottom = child.Outer.Top;
         }
-      
+
 
         _divList.Add(parent);
         _divList.Add(child);
         SplitDivison(!isVertical);
-       
-        
+
+
     }
 
-    private void CreateRoom() 
+    private void CreateRoom()
     {
-        foreach (var div in _divList) 
+        foreach (var div in _divList)
         {
             //部屋の基準のサイズを決める
             int dw = div.Outer.Width() - _outerMergin;
@@ -253,7 +275,7 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
         {
             for (int i = room.Top; i <= room.Bottom; i++)
             {
-                Layer.SetData(j, i, 1);
+                Layer.SetData(j, i, MapNum.LoadNum);
             }
         }
 
@@ -264,22 +286,22 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
     /// </summary>
     /// <param name="a"></param>
     /// <returns></returns>
-    private bool CheckDivisionSize(int a) 
+    private bool CheckDivisionSize(int a)
     {
         //指定の高さより低かった場合false
         return a <= _minRoom;
     }
 
-    private void ConnectRooms() 
+    private void ConnectRooms()
     {
 
-        for (int i = 0; i < _divList.Count - 1; i++) 
+        for (int i = 0; i < _divList.Count - 1; i++)
         {
             DgDivision a = _divList[i];
             DgDivision b = _divList[i + 1];
 
             CreateRoad(a, b);
-           
+
         }
     }
 
@@ -288,14 +310,14 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
     /// </summary>
     /// <param name="a"></param>
     /// <param name="b"></param>
-    private void CreateRoad(DgDivision a, DgDivision b) 
+    private void CreateRoad(DgDivision a, DgDivision b)
     {
         if (a.Outer.Right == b.Outer.Left)
         {
             //横に伸ばす
             CreateHorizontalRoad(a, b);
         }
-        else 
+        else
         {
             //縦に伸ばす
             CreateVerticalRoad(a, b);
@@ -307,58 +329,58 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
     /// </summary>
     /// <param name="a"></param>
     /// <param name="b"></param>
-    private void CreateHorizontalRoad(DgDivision a, DgDivision b) 
+    private void CreateHorizontalRoad(DgDivision a, DgDivision b)
     {
         int y1 = Random.Range(a.Room.Top, a.Room.Bottom);
         int y2 = Random.Range(b.Room.Top, b.Room.Bottom);
 
-        for (int x = a.Room.Right; x < a.Outer.Right; x++) 
+        for (int x = a.Room.Right; x < a.Outer.Right; x++)
         {
-            Layer.SetData(x, y1, 1);
+            Layer.SetData(x, y1, MapNum.LoadNum);
         }
-        for (int x = b.Room.Left; x > b.Outer.Left; x--) 
+        for (int x = b.Room.Left; x > b.Outer.Left; x--)
         {
-            Layer.SetData(x, y2, 1);
+            Layer.SetData(x, y2, MapNum.LoadNum);
         }
-        for (int y = Mathf.Min(y1, y2), end = Mathf.Max(y1, y2); y <= end; y++) 
+        for (int y = Mathf.Min(y1, y2), end = Mathf.Max(y1, y2); y <= end; y++)
         {
-            Layer.SetData(a.Outer.Right, y, 1);
+            Layer.SetData(a.Outer.Right, y, MapNum.LoadNum);
         }
     }
 
-    
+
     /// <summary>
     /// 縦につなぐ道を作る
     /// </summary>
     /// <param name="a"></param>
     /// <param name="b"></param>
-    private void CreateVerticalRoad(DgDivision a, DgDivision b) 
+    private void CreateVerticalRoad(DgDivision a, DgDivision b)
     {
         int x1 = Random.Range(a.Room.Left, a.Room.Right);
         int x2 = Random.Range(b.Room.Left, b.Room.Right);
 
-        for (int y = a.Room.Bottom; y < a.Outer.Bottom; y++) 
+        for (int y = a.Room.Bottom; y < a.Outer.Bottom; y++)
         {
-            Layer.SetData(x1, y, 1);
+            Layer.SetData(x1, y, MapNum.LoadNum);
         }
-        for (int y = b.Room.Top; y > b.Outer.Top; y--) 
+        for (int y = b.Room.Top; y > b.Outer.Top; y--)
         {
-            Layer.SetData(x2, y, 1);
+            Layer.SetData(x2, y, MapNum.LoadNum);
         }
-        for (int x = Mathf.Min(x1, x2), end = Mathf.Max(x1, x2); x <= end; x++) 
+        for (int x = Mathf.Min(x1, x2), end = Mathf.Max(x1, x2); x <= end; x++)
         {
-            Layer.SetData(x, a.Outer.Bottom, 1);
+            Layer.SetData(x, a.Outer.Bottom, MapNum.LoadNum);
         }
     }
 
     /// <summary>
     /// タイルチップをセットする
     /// </summary>
-    private void SetTile() 
+    private void SetTile()
     {
-        for (int y = 0; y < Layer.Height; ++y) 
+        for (int y = 0; y < Layer.Height; ++y)
         {
-            for (int x = 0; x < Layer.Width; ++x) 
+            for (int x = 0; x < Layer.Width; ++x)
             {
                 //XYの値を入れて配列の中身の数字を持ってくる
                 int a = Layer.GetMapData(x, y);
@@ -367,11 +389,13 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
                 if (a == 1)
                 {
                     var v = Instantiate(_kusanyaObject, new Vector3(x, -1 * y, 0), _kusanyaObject.transform.rotation);
+                    v.transform.parent = _mapParent.transform;
                     //TrapGenerator(x, y);
                 }
-                else if(a == 0) 
+                else if (a == 0)
                 {
                     var t = Instantiate(_iwaObject, new Vector3(x, -1 * y, 0), _iwaObject.transform.rotation);
+                    t.transform.parent = _mapParent.transform;
                 }
             }
         }
@@ -394,7 +418,7 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
     /// オブジェクトを入れるとダンジョン内にランダムに生成する
     /// </summary>
     /// <param name="Iobject">インスタンスするオブジェクト</param>
-    public void Generatesomething(GameObject Iobject) 
+    public void Generatesomething(GameObject Iobject)
     {
         //ランダムな区画を選択する
         int suffix = Random.Range(0, _divList.Count);
@@ -405,38 +429,39 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
 
         var Object = Instantiate(Iobject, new Vector3(x, -1 * y, 0), _stairSet.transform.rotation);
 
-        if (Iobject == _playerObject) 
-        {
-            GameManager.Instance.SetPlayerPosition(x, y);
-        }
+        Object.transform.parent = _mapParent.transform;
 
-        if (Iobject == _enemyPrefab) 
+        if (Iobject == _enemyPrefab)
         {
             //エネミーに今自分がどの部屋にいるか教えてあげる
             var EnemyScript = Object.GetComponent<EnemyBase>();
             //EnemyScript.SetRoomNum(suffix);
             EnemyManager.Instance.SetTotalEnemyNum(1);
         }
+        else if(Iobject == _stairSet) 
+        {
+            Layer.SetData(x, y, MapNum.StairNum);
+        }
     }
 
     /// <summary>
     /// ランダムな区画にアイテムを生成する
     /// </summary>
-    public void ItemGeneratesomething() 
+    public void ItemGeneratesomething()
     {
-        foreach (var i in _divList) 
+        foreach (var i in _divList)
         {
             //部屋の中にランダムな数のアイテムを生成する
             int ItemNum = Random.Range(0, 3);
             for (int j = 0; j < ItemNum; j++)
             {
                 //アイテムの座標をかぶらせないために一度生成したものをリストに保存
-            　　List<int> PosList = new List<int>();
+                List<int> PosList = new List<int>();
                 //部屋の中のランダムな座標を指定
                 int x = Random.Range(i.Room.Left, i.Room.Right);
                 int y = Random.Range(i.Room.Top, i.Room.Bottom);
                 //被らない座標が出るまで回し続ける
-                while(PosList.Contains(x + y))
+                while (PosList.Contains(x + y))
                 {
                     x = Random.Range(i.Room.Left, i.Room.Right);
                     y = Random.Range(i.Room.Top, i.Room.Bottom);
@@ -446,6 +471,7 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
                 PosList.Add(x + y);
 
                 var ItemObject = Instantiate(_itemPrefab, new Vector3(x, -1 * y, 0), _itemPrefab.transform.rotation);
+                ItemObject.transform.parent = _mapParent.transform;
                 //インスタンス化したプレハブからスクリプトを取得する
                 var ItemObjectCs = ItemObject.GetComponent<ItemObjectScript>();
                 //データーベースの中からランダムなアイテムを取ってくる
@@ -456,7 +482,7 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
                 ItemObjectCs.SetItemSprite(ItemRan.GetItemImage);
 
                 //リストにアイテムのオブジェクトをセットする
-                GameManager.Instance.SetItemObjList(ItemObject);   
+                GameManager.Instance.SetItemObjList(ItemObject);
             }
         }
     }
@@ -466,19 +492,39 @@ public class DgGenerator : SingletonBehaviour<DgGenerator>
     /// </summary>
     private void TrapGenerator(int x, int y)
     {
-        if (Random.Range(0, 101) > 95) 
+        if (Random.Range(0, 101) > 95)
         {
             var v = Instantiate(_trapPrefab, new Vector3(x, -1 * y, 0), _kusanyaObject.transform.rotation);
-        } 
+        }
     }
 
     /// <summary>
     /// DivListを返す
     /// </summary>
     /// <returns></returns>
-    public List<DgDivision> GetDivList() 
+    public List<DgDivision> GetDivList()
     {
         return _divList;
     }
-    
+
+    /// <summary>設定を初期化する</summary>
+    private void MapInit() 
+    {
+        Layer = null;
+        Destroy(_mapParent);
+        _mapParent = Instantiate(_mapParentPrefab, new Vector3(0, 0, 0), transform.rotation);
+    }
+
+}
+
+public static class MapNum 
+{
+    /// <summary>壁</summary>
+    public const int WallNum = 0;
+    /// <summary></summary>
+    public const int LoadNum = 1;
+    /// <summary>アイテム</summary>
+    public const int ItemNum = 3;
+    /// <summary>階段</summary>
+    public const int StairNum = 4;
 }
