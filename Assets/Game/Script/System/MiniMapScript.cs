9cd9115bd6d4524b6a676f24755bdb948bce3d6a
sakamotosyo <syo10150000@gmail.com>
2022-09-06 19:36:37 +0900

    [Update]Colliderで判定をとれるように変更

--- Assets/Game/Script/System/MiniMapScript.cs
+++ Assets/Game/Script/System/MiniMapScript.cs
@@ -4,38 +4,31 @@ using UnityEngine;
 
 public class MiniMapScript : MonoBehaviour
 {
-    GameManager _gameManager;
-
-    [Header("どのくらいPLあいぇｒの周りを明るくするか")]
+    [Header("どのくらいPlayerの周りを明るくするか")]
     [SerializeField] int _searchNum = 0;
 
     [Tooltip("MiniMapに表示するためのObject")]
     [SerializeField] GameObject _miniMapObj;
-    // Start is called before the first frame update
-    void Start()
+
+    void OnTriggerEnter2D(Collider2D collision)
     {
-        _gameManager = GameManager.Instance;
-        _gameManager.PlayerObj.GetComponent<PlayerMove>().MiniMapUpdate += MapUpdate;
-
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-    /// <summary>Playerが移動したときにMapを更新する</summary>
-    void MapUpdate() 
-    {
-        var PosNum  = Mathf.Abs(_gameManager.PlayerObj.transform.position.x - transform.position.x) 
-            + Mathf.Abs(_gameManager.PlayerObj.transform.position.y - transform.position.y);
-
-        if (PosNum < _searchNum) 
+        if (collision.gameObject.tag == "Player") 
         {
             _miniMapObj.SetActive(false);
         }
-
+        
     }
 }
+
+    ///// <summary>Playerが移動したときにMapを更新する</summary>
+    //void MapUpdate() 
+    //{
+    //    var PosNum  = Mathf.Abs(_gameManager.PlayerObj.transform.position.x - transform.position.x) 
+    //        + Mathf.Abs(_gameManager.PlayerObj.transform.position.y - transform.position.y);
+
+    //    if (PosNum < _searchNum) 
+    //    {
+    //        _miniMapObj.SetActive(false);
+    //    }
+
+    //}
