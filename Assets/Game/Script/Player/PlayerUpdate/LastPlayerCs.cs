ba2bbc191bc2f4a1b42cfcf4dc3792437ac479e8
sakamotosyo <syo10150000@gmail.com>
2022-09-21 03:09:26 +0900

    [Update]ボス戦に入った時の処理を追加

--- Assets/Game/Script/Player/PlayerUpdate/LastPlayerCs.cs
+++ Assets/Game/Script/Player/PlayerUpdate/LastPlayerCs.cs
@@ -6,20 +6,39 @@ public class LastPlayerCs : MonoBehaviour
 {
     [Header("アニメーション")]
     [SerializeField] Animator _animator;
-
+    [Header("GSSReaderのスクリプト")]
     [SerializeField] GSSReader _gssReader;
+
+    [Tooltip("GameManagerのインスタンス")]
+    GameManager _gameManager;
+    [Tooltip("DgGeneratorのインスタンス")]
+    DgGenerator _dgGenerator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        _gameManager = GameManager.Instance;
+        _dgGenerator = DgGenerator.Instance;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (GameManager.Instance.TurnType == GameManager.TurnManager.Player) 
+        if (_gameManager.TurnType == GameManager.TurnManager.Story) 
         {
             _animator.SetBool("Move", true);
         }
+    }
+
+    /// <summary>
+    /// ボス戦をスタートする
+    /// </summary>
+    public void BossStart() 
+    {
+        _gameManager.SetPlayerPosition((int)transform.position.x, (int)transform.position.y * -1);
+        _gameManager.PlayerObj.transform.position = transform.position;
+        _gameManager.PlayerObj.SetActive(true);
+        gameObject.SetActive(false);
+       
+        _gameManager.TurnType = GameManager.TurnManager.Player;
     }
 }
