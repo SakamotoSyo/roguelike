565c0b632d34430d4ad64c0d9e070a8fda9a092a
sakamotosyo <syo10150000@gmail.com>
2022-09-21 03:05:46 +0900

    向いている方向を取得する機能を持たせるInterfaceの作成

---
+++ Assets/Game/Script/Interface/IDirection.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface IDirection
+{
+    /// <summary>
+    /// Œü‚¢‚Ä‚¢‚é•ûŒü‚ðŽæ“¾‚·‚é
+    /// </summary>
+    /// <returns></returns>
+    public Vector2 GetDirection();
+
+}
