ff01ce49d06a5d2114634b18c9d1d5bdaf456bba
sakamotosyo <syo10150000@gmail.com>
2022-10-02 01:37:20 +0900

    [Update]コメントアウト化してエラー修正

--- Assets/Game/Script/GSSReader/GSSReaderEditor.cs
+++ Assets/Game/Script/GSSReader/GSSReaderEditor.cs
@@ -1,18 +1,18 @@
-using UnityEngine;
-using UnityEditor;
-[CustomEditor(typeof(GSSReader))]
-public class GSSReaderInspector : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
-        var r = target as GSSReader;
-        var t = "スプレッドシート読み込み";
-        EditorGUI.BeginDisabledGroup(r.IsLoading);
-        if (GUILayout.Button(t))
-        {
-            r.Reload();
-        }
-        EditorGUI.EndDisabledGroup();
-    }
-}
+//using UnityEngine;
+//using UnityEditor;
+//[CustomEditor(typeof(GSSReader))]
+//public class GSSReaderInspector : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
+//        base.OnInspectorGUI();
+//        var r = target as GSSReader;
+//        var t = "スプレッドシート読み込み";
+//        EditorGUI.BeginDisabledGroup(r.IsLoading);
+//        if (GUILayout.Button(t))
+//        {
+//            r.Reload();
+//        }
+//        EditorGUI.EndDisabledGroup();
+//    }
+//}
